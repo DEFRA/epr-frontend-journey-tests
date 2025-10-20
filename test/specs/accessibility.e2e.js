c2bee60c3c0b31744fc9574839ad89abe8bea697
Antony Teoh <antony.teoh@gmail.com>
2025-10-20 14:28:50 +0100

    PAE-224: Add accessibility checks to epr-frontend

---
+++ test/specs/accessibility.e2e.js
@@ -0,0 +1,46 @@
+import EprFrontendHomePage from 'page-objects/homepage.js'
+import { browser } from '@wdio/globals'
+
+import AxeBuilder from '@axe-core/webdriverio'
+import WasteRecordsPage from 'page-objects/waste.records.page.js'
+import UploadSummaryLogPage from 'page-objects/upload.summary.log.page.js'
+import { logViolationsToAllure } from '../support/accessibility.js'
+
+function failOnViolationLevel(results) {
+  results.violations.forEach((violation) => {
+    if (violation.impact === 'critical' || violation.impact === 'serious') {
+      expect.fail(
+        'At least one Serious or Critical accessibility violation found'
+      )
+    }
+  })
+}
+
+describe('WCAG Accessibility', () => {
+  it('Should have no critical accessibility violations for Home Page', async () => {
+    await EprFrontendHomePage.open()
+
+    const builder = new AxeBuilder({ client: browser })
+    const results = await builder.analyze()
+    await logViolationsToAllure(results.violations)
+    failOnViolationLevel(results)
+  })
+
+  it('Should have no critical accessibility violations for Packaging Waste Records Page', async () => {
+    await WasteRecordsPage.open('123', '456')
+
+    const builder = new AxeBuilder({ client: browser })
+    const results = await builder.analyze()
+    await logViolationsToAllure(results.violations)
+    failOnViolationLevel(results)
+  })
+
+  it('Should have no critical accessibility violations for Upload Summary Logs Page', async () => {
+    await UploadSummaryLogPage.open('123', '456')
+
+    const builder = new AxeBuilder({ client: browser })
+    const results = await builder.analyze()
+    await logViolationsToAllure(results.violations)
+    failOnViolationLevel(results)
+  })
+})
