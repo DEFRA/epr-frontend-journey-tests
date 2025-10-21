5f6c12a0d2a5d5e851f988c62b24469d10f0f1b9
Antony Teoh <antony.teoh@gmail.com>
2025-10-21 17:25:57 +0100

    Check for change in state on UI when file is uploaded (#42)
    * Check for change in state on UI when file is uploaded
    
    * Extract long methods into function

---
+++ test/support/checks.js
@@ -0,0 +1,31 @@
+import { browser } from '@wdio/globals'
+
+export async function checkBodyText(message, timeoutInSeconds) {
+  await browser.waitUntil(
+    async () => {
+      const pageText = await browser.$('body').getText()
+      return pageText.includes(message)
+    },
+    {
+      timeout: timeoutInSeconds * 1000,
+      timeoutMsg: `Expected text "${message}" to be present on the page within ${timeoutInSeconds} seconds`
+    }
+  )
+}
+
+export async function checkUploadErrorText(
+  selector,
+  message,
+  timeoutInSeconds
+) {
+  await browser.waitUntil(
+    async () => {
+      const errorText = await browser.$(selector).getText()
+      return errorText.includes(message)
+    },
+    {
+      timeout: timeoutInSeconds * 1000,
+      timeoutMsg: `Expected error message "${message}" to be present on the page within ${timeoutInSeconds} seconds`
+    }
+  )
+}
