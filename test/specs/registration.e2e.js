36989936662a6a5af1a68930b61816128154f4b6
Antony Teoh <antony.teoh@gmail.com>
2025-10-21 15:13:31 +0100

    Extract long methods into function

--- test/specs/registration.e2e.js
+++ test/specs/registration.e2e.js
@@ -1,6 +1,7 @@
 import { browser, expect } from '@wdio/globals'
 import WasteRecordsPage from '../page-objects/waste.records.page.js'
 import UploadSummaryLogPage from '../page-objects/upload.summary.log.page.js'
+import { checkBodyText, checkUploadErrorText } from '../support/checks.js'
 
 describe('Registration', () => {
   it('Should be able to submit Summary Log spreadsheet', async () => {
@@ -12,28 +13,8 @@ describe('Registration', () => {
     )
     await UploadSummaryLogPage.uploadFile('resources/sample.xlsx')
     await UploadSummaryLogPage.continue()
-    await browser.waitUntil(
-      async () => {
-        const pageText = await browser.$('body').getText()
-        return pageText.includes('Your file is being uploaded')
-      },
-      {
-        timeout: 5000,
-        timeoutMsg:
-          'Expected text "Your file is being uploaded" to be present on the page within 5 seconds'
-      }
-    )
-    await browser.waitUntil(
-      async () => {
-        const pageText = await browser.$('body').getText()
-        return pageText.includes('Your file is ready to submit')
-      },
-      {
-        timeout: 10000,
-        timeoutMsg:
-          'Expected text "Your file is ready to submit" to be present on the page within 10 seconds'
-      }
-    )
+    await checkBodyText('Your file is being uploaded', 5)
+    await checkBodyText('Your file is ready to submit', 10)
   })
 
   it('Should get an error message with an empty Summary Log spreadsheet', async () => {
@@ -43,16 +24,10 @@ describe('Registration', () => {
     )
     await UploadSummaryLogPage.uploadFile('resources/empty.xlsx')
     await UploadSummaryLogPage.continue()
-    await browser.waitUntil(
-      async () => {
-        const errorText = await browser.$('#summary-log-upload-error').getText()
-        return errorText.includes('The selected file is empty')
-      },
-      {
-        timeout: 5000,
-        timeoutMsg:
-          'Expected error message "The selected file is empty" to be present on the page within 5 seconds'
-      }
+    await checkUploadErrorText(
+      '#summary-log-upload-error',
+      'The selected file is empty',
+      5
     )
   })
 })
