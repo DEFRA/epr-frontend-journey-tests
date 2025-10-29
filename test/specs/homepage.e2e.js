d94cd059c3a5983fe0ad125ef03c55d6d5e14ae9
Antony Teoh <antony.teoh@gmail.com>
2025-10-29 14:33:04 +0000

    Re-enable tests

--- test/specs/homepage.e2e.js
+++ test/specs/homepage.e2e.js
@@ -1,16 +1,16 @@
-// import EprFrontendHomePage from '../page-objects/homepage.js'
-//
-// import { browser, expect } from '@wdio/globals'
-//
-// describe('EPR Frontend', () => {
-//   it('Should be able to navigate to Home Page', async () => {
-//     await EprFrontendHomePage.open()
-//     await expect(browser).toHaveTitle(expect.stringContaining('Home'))
-//   })
-//
-//   it('Should be able to navigate to Home Page via Welsh', async () => {
-//     await EprFrontendHomePage.open('/cy')
-//     const lang = await browser.$('html').getAttribute('lang')
-//     expect(lang).toBe('cy')
-//   })
-// })
+import EprFrontendHomePage from '../page-objects/homepage.js'
+
+import { browser, expect } from '@wdio/globals'
+
+describe('EPR Frontend', () => {
+  it('Should be able to navigate to Home Page', async () => {
+    await EprFrontendHomePage.open()
+    await expect(browser).toHaveTitle(expect.stringContaining('Home'))
+  })
+
+  it('Should be able to navigate to Home Page via Welsh', async () => {
+    await EprFrontendHomePage.open('/cy')
+    const lang = await browser.$('html').getAttribute('lang')
+    expect(lang).toBe('cy')
+  })
+})
