bdb184c977d5ac60033ee8f95971fedf7f58461c
Antony Teoh <antony.teoh@gmail.com>
2025-10-31 13:57:06 +0000

    PAE-445: Update Welsh translation test (#53)

--- test/specs/homepage.e2e.js
+++ test/specs/homepage.e2e.js
@@ -3,14 +3,10 @@ import EprFrontendHomePage from '../page-objects/homepage.js'
 import { browser, expect } from '@wdio/globals'
 
 describe('EPR Frontend', () => {
-  it('Should be able to navigate to Home Page', async () => {
-    await EprFrontendHomePage.open()
-    await expect(browser).toHaveTitle(expect.stringContaining('Home'))
-  })
-
   it('Should be able to navigate to Home Page via Welsh', async () => {
     await EprFrontendHomePage.open('/cy')
     const lang = await browser.$('html').getAttribute('lang')
     expect(lang).toBe('cy')
+    await expect(browser).toHaveTitle(expect.stringContaining('Hafan'))
   })
 })
