81e47828ceb03cadf1739a31502042adb967ac12
Tom Halley <2283407+tomhalley@users.noreply.github.com>
2025-12-02 10:31:14 +0000

    Replaced axe webdriver with lighthouse

--- test/specs/accessibility.e2e.js
+++ test/specs/accessibility.e2e.js
@@ -1,7 +1,6 @@
 import HomePage from 'page-objects/homepage.js'
-import { browser } from '@wdio/globals'
 
-import AxeBuilder from '@axe-core/webdriverio'
+import { runAccessibilityAudit } from '../support/chrome.accessibility.js'
 import WasteRecordsPage from 'page-objects/waste.records.page.js'
 import UploadSummaryLogPage from 'page-objects/upload.summary.log.page.js'
 import { logViolationsToAllure } from '../support/accessibility.js'
@@ -20,8 +19,7 @@ describe('WCAG Accessibility', () => {
   it('Should have no critical accessibility violations for Home Page', async () => {
     await HomePage.open()
 
-    const builder = new AxeBuilder({ client: browser })
-    const results = await builder.analyze()
+    const results = await runAccessibilityAudit()
     await logViolationsToAllure(results.violations)
     failOnViolationLevel(results)
   })
@@ -29,8 +27,7 @@ describe('WCAG Accessibility', () => {
   it('Should have no critical accessibility violations for Packaging Waste Records Page', async () => {
     await WasteRecordsPage.open('123', '456')
 
-    const builder = new AxeBuilder({ client: browser })
-    const results = await builder.analyze()
+    const results = await runAccessibilityAudit()
     await logViolationsToAllure(results.violations)
     failOnViolationLevel(results)
   })
@@ -38,8 +35,7 @@ describe('WCAG Accessibility', () => {
   it('Should have no critical accessibility violations for Upload Summary Logs Page', async () => {
     await UploadSummaryLogPage.open('123', '456')
 
-    const builder = new AxeBuilder({ client: browser })
-    const results = await builder.analyze()
+    const results = await runAccessibilityAudit()
     await logViolationsToAllure(results.violations)
     failOnViolationLevel(results)
   })
