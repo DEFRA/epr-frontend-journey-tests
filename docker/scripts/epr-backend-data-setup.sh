0b6ffb91872b40ded8c819e76af673c12e5862b6
Antony Teoh <antony.teoh@gmail.com>
2025-12-08 20:33:42 +0000

    Add data setup to frontend journey test

---
+++ docker/scripts/epr-backend-data-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+curl -X PATCH http://epr-backend:3001/v1/dev/organisations/6507f1f77bcf86cd79943911 \
+  -H "Content-Type: application/json" \
+  -d '{
+        "organisation": {
+          "registrations": [
+            {
+              "id": "6507f1f77bcf86cd79943912",
+              "status": "approved",
+              "validFrom": "2025-01-01T00:00:00.000Z",
+              "validTo": "2026-01-01T00:00:00.000Z",
+              "registrationNumber": "R25SR500030912PA"
+            },
+            {
+              "id": "6507f1f77bcf86cd79943913",
+              "status": "approved",
+              "validFrom": "2025-01-01T00:00:00.000Z",
+              "validTo": "2026-01-01T00:00:00.000Z",
+              "registrationNumber": "R25SR500030913PA"
+            }
+          ],
+          "accreditations": [
+            {
+              "id": "68f6a147c117aec8a1ab7497",
+              "accreditationNumber": "ACC123456",
+              "status": "approved"
+            }
+          ]
+        }
+      }
+'
