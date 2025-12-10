c87a5478b5262d276dde62e770042091f5f2f6ee
Antony Teoh <antony.teoh@gmail.com>
2025-12-10 20:05:15 +0000

    Update script

--- docker/scripts/epr-backend-data-setup.sh
+++ docker/scripts/epr-backend-data-setup.sh
@@ -9,14 +9,14 @@ curl -X PATCH http://epr-backend:3001/v1/dev/organisations/6507f1f77bcf86cd79943
               "id": "6507f1f77bcf86cd79943912",
               "status": "approved",
               "validFrom": "2025-01-01T00:00:00.000Z",
-              "validTo": "2026-01-01T00:00:00.000Z",
+              "validTo": "2028-01-01T00:00:00.000Z",
               "registrationNumber": "R25SR500030912PA"
             },
             {
               "id": "6507f1f77bcf86cd79943913",
               "status": "approved",
               "validFrom": "2025-01-01T00:00:00.000Z",
-              "validTo": "2026-01-01T00:00:00.000Z",
+              "validTo": "2028-01-01T00:00:00.000Z",
               "registrationNumber": "R25SR500030913PA"
             }
           ],
@@ -24,6 +24,8 @@ curl -X PATCH http://epr-backend:3001/v1/dev/organisations/6507f1f77bcf86cd79943
             {
               "id": "68f6a147c117aec8a1ab7497",
               "accreditationNumber": "ACC123456",
+              "validFrom": "2025-01-01T00:00:00.000Z",
+              "validTo": "2028-01-01T00:00:00.000Z",
               "status": "approved"
             }
           ]
