0e38f07ac211d2fe2f5c1bad5b073f7e776f7202
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2025-12-18 18:49:45 +0000

    PAE-658: Update success page text assertion (#87)

--- test/specs/registration.e2e.js
+++ test/specs/registration.e2e.js
@@ -61,10 +61,7 @@ describe('Registration', () => {
     await checkBodyText('Check before confirming upload', 10)
     await UploadSummaryLogPage.confirmAndSubmit()
 
-    await checkBodyText(
-      'Summary log for accreditation number ACC123456 submitted',
-      10
-    )
+    await checkBodyText('Summary log uploaded', 10)
 
     await Homepage.signOut()
     await expect(browser).toHaveTitle(expect.stringContaining('Home'))
