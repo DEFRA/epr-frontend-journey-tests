80505fc51478de94d3204d9e7cf6d6cd86e1e4a0
Graeme Foster <80714+GraemeF@users.noreply.github.com>
2026-01-07 14:55:15 +0000

    PAE-793: Skip Welsh homepage test until translations are available (#96)

--- test/specs/homepage.e2e.js
+++ test/specs/homepage.e2e.js
@@ -3,7 +3,8 @@ import HomePage from '../page-objects/homepage.js'
 import { browser, expect } from '@wdio/globals'
 
 describe('EPR Frontend', () => {
-  it('Should be able to navigate to Home Page via Welsh', async () => {
+  // TODO: Re-enable when Welsh translations are available (PAE-793)
+  it.skip('Should be able to navigate to Home Page via Welsh', async () => {
     await HomePage.open('/cy')
     const lang = await browser.$('html').getAttribute('lang')
     expect(lang).toBe('cy')
