ee27bb27756a7bf0ff18b393de6b273f022371b2
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-06 20:50:28 +0000

    PAE-000: run tests on matching frontend branch

--- wdio.github.conf.js
+++ wdio.github.conf.js
@@ -24,10 +24,11 @@ export const config = {
   specs: ['./test/specs/**/*.js'],
   // Tests to exclude
   exclude: [],
-  maxInstances: 1,
+  maxInstances: 3,
 
   capabilities: [
     {
+      maxInstances: 3,
       browserName: 'chrome',
       'goog:chromeOptions': {
         args: [
