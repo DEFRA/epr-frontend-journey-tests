fab8406dd633a54905b51ef74099257aa6921c6d
David Lusby <1230979+loveboat@users.noreply.github.com>
2026-02-07 13:39:02 +0000

    revert get-text change

--- test/support/checks.js
+++ test/support/checks.js
@@ -2,8 +2,10 @@ import { browser } from '@wdio/globals'
 
 export async function checkBodyText(message, timeoutInSeconds) {
   await browser.waitUntil(
-    async () =>
-      browser.execute((msg) => document.body.innerText.includes(msg), message),
+    async () => {
+      const pageText = await browser.$('body').getText()
+      return pageText.includes(message)
+    },
     {
       timeout: timeoutInSeconds * 1000,
       timeoutMsg: `Expected text "${message}" to be present on the page within ${timeoutInSeconds} seconds`
