2e4f7916ed3e7ca5404be8b3eedcedd96c21ef5a
Antony Teoh <antony.teoh@gmail.com>
2026-02-18 15:47:13 +0000

    PAE-1113: Update test to use a Large Producer waste org with different trading name (#195)

--- test/support/fixtures.js
+++ test/support/fixtures.js
@@ -1,4 +1,7 @@
 export const tradingName = 'CS_GENERATED_3982709_England'
-export const secondTradingName = 'CS_GENERATED_3792475_England'
+
+// Actual trading name is test10 for this waste org, but as its a Large Producer, the organisation name is used instead
+export const secondTradingName = 'Ball Corporation'
+
 export const thirdTradingName =
   '13 MARLBOROUGH BUILDINGS (BATH) MANAGEMENT COMPANY LTD'
