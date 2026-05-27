35a7c9b7f5f2ad1a293c04f11ade60d813e43e3e
Antony Teoh <antony.teoh@gmail.com>
2026-05-27 17:58:51 +0100

    Fix lint issue

--- test/support/checks.js
+++ test/support/checks.js
@@ -3,9 +3,11 @@ import { browser } from '@wdio/globals'
 export async function checkBodyText(message, timeoutInSeconds) {
   await browser.waitUntil(
     async () =>
-      browser.execute(function (msg) {
-        return document.body.innerText.includes(msg)
-      }, message),
+      browser.execute(
+        /** @type {any} */ (
+          (msg) => document.body.innerText.includes(msg), message
+        )
+      ),
     {
       timeout: timeoutInSeconds * 1000,
       timeoutMsg: `Expected text "${message}" to be present on the page within ${timeoutInSeconds} seconds`
