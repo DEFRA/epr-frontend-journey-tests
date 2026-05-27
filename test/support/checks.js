3265d097f3d2cf2dba1c1f96a42dcc68d9c9fee7
Antony Teoh <antony.teoh@gmail.com>
2026-05-27 18:03:10 +0100

    Revert change

--- test/support/checks.js
+++ test/support/checks.js
@@ -3,11 +3,7 @@ import { browser } from '@wdio/globals'
 export async function checkBodyText(message, timeoutInSeconds) {
   await browser.waitUntil(
     async () =>
-      browser.execute(
-        /** @type {any} */ (
-          (msg) => document.body.innerText.includes(msg), message
-        )
-      ),
+      browser.execute((msg) => document.body.innerText.includes(msg), message),
     {
       timeout: timeoutInSeconds * 1000,
       timeoutMsg: `Expected text "${message}" to be present on the page within ${timeoutInSeconds} seconds`
