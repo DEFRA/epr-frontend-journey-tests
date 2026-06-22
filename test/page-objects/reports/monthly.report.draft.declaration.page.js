c18a369e3b4476734bad3852271f223e3b44e885
Senthil Kumar Arumugam <16401283+senthilinfy@users.noreply.github.com>
2026-06-22 14:12:45 +0100

    PAE-1476: Update journey tests for submissionDeclaredBy on declaration page (#380)

--- test/page-objects/reports/monthly.report.draft.declaration.page.js
+++ test/page-objects/reports/monthly.report.draft.declaration.page.js
@@ -6,13 +6,19 @@ class MonthlyReportDraftDeclarationPage {
     await $('a*=Reports').click()
   }
 
-  async confirmAndSubmit() {
+  async enterFullName(name) {
+    await $('#submissionDeclaredBy').setValue(name)
+  }
+
+  async confirmAndSubmit(name = 'Test User') {
+    await this.enterFullName(name)
     const buttonElement = await $('#main-content button[type=submit]')
     await buttonElement.waitForClickable({ timeout: 5000 })
     await buttonElement.click()
   }
 
-  async submitAndCheckDoubleClickPrevented() {
+  async submitAndCheckDoubleClickPrevented(name = 'Test User') {
+    await this.enterFullName(name)
     await checkDoubleClickPrevented('#main-content button[type=submit]')
   }
 
